c8ac285bdd2f98edf5a9ccd8afda963d46d18caa
Jacqueline Walsh <jacqueline-NoWebCam@jacqueline-mac.home>
2022-10-26 09:17:50 +0100

    update profile

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -6,29 +6,29 @@ const Profile = () => {
     <div className="profile-container">
       <h3 className="profile-subheading">PROFILE</h3>
       <p className="profile-paragraph">
-        I was originally born in a small village in Yorkshire, England. In the
-        late 1980s, I left England to begin a career in finance in Jersey,
-        Channel Islands. I moved to my current home in Galway, Ireland in 2005,
-        where I took a parental break to raise my two children.
+        I was originally born in a small village in Yorkshire, England. I left
+        England to begin a career in Finance in Jersey, Channel Islands and
+        finally moved to my current home in Galway, Ireland in 2005, where I
+        took a parental break to raise my two children.
       </p>
       <p className="profile-paragraph">
-        While taking a parental break, I studied the field of software
-        development, where my enthusiasm to gain knowledge took me on a long and
+        Whilst taking a parental break, I studied the field of Software
+        Development, where my enthusiasm to gain knowledge took me on a long and
         exciting journey. In 2021, I joined the Oracle Digital Experience Team
         in Dublin as a React Frontend Developer.
       </p>
       <p className="profile-paragraph">
         As a Frontend Developer working in an agile team environment, I am
         responsible for building and refactoring React components. Each
-        component is unit tested using the Jest Library and has a coresponding
-        storybook component which provides an extensive visual library for the
-        end user. Working from home, I am lucky to be able to connect and engage
-        with a team worldwide. This has provided a warm cultural and team
+        component is unit tested using the Jest library and correlates to a
+        matching storybook component to provide an extensive visual library for
+        the end user. Working from home, I am lucky to be able to connect and
+        engage with a team worldwide. This has provided a warm cultural and team
         comradery that makes my job such a joy.
       </p>
       <p className="profile-paragraph">
-        Through continuous development over the years, I have gained knowledge
-        in many areas, including languages such as Java and Python. I have
+        Through continuous learning over the years, I have gained knowledge in
+        many areas, including languages such as Java and Python. I have
         developed with frameworks such as React, Angular, Flask, and Django and
         have added backend solutions with Node.js and Python. I have worked with
         both MySQL and NoSQL databases, gaining qualification as an Oracle MySQL
